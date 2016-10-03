0281c58f819a63f403b3f072be0d07cddd064986
Riad Hacini <riad.hacini.pro@gmail.com>
2016-10-03 11:29:04 +0200

    Footer fini reste le responsive a gerer

--- public/footer.php
+++ public/footer.php
@@ -6,8 +6,41 @@
  */
 ?>
 
-<footer class="container-fluid">
+<footer class="container-fluid" id="footer-global">
     <div class="container">
+        <div class="col-xs-12" class="wrap-footer">
 
+            <div class="col-xs-12 col-md-3">
+                <a href="index.php"><img id="logo-footer" src="img/logo-klap-affaires.png"></a>
+            </div>
+
+            <div class="col-xs-12 col-md-6">
+                <div class="col-xs-12" id="call-to-action">
+
+                    <div class="col-xs-12 col-md-6" class="call-to-action">
+                        <p>Suivez-nous sur les réseaux sociaux:</p>
+                        <ul class="social">
+                            <li><img class="social-icons" src="img/logo-youtube-klap-affaires.png"></li>
+                            <li><img class="social-icons" src="img/logo-facebook-klap-affaires.png"></li>
+                            <li><img class="social-icons" src="img/logo-twitter-klap-affaires.png"></li>
+                            <li><img class="social-icons" src="img/logo-linkedin-klap-affaires.png"></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-xs-12 col-md-6" class="call-to-action">
+                        <p>Discutons ensemble de votre projet :</p>
+                        <a href="contact.php"> <button id="button-footer">Contact</button></a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+        <div class="col-xs-12 footer-content" class="call-to-action">
+            <p>©Copyright Klap'Affaires<br />Tous droits réservés.<br /><a id="mentions-legales" href="#">Mentions Légales </a></p>
+        </div>
+        <div class="col-xs-12" class="wrap-footer">
+
+        </div>
     </div>
 </footer>
