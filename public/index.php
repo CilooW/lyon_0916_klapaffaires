027311394ad3717547ed995fc6dc38d513dc5d2e
Nissa69 <yann.chanteloup.pro@gmail.com>
2016-10-03 10:58:44 +0200

    Modif du css index et ajout de texte sous la video et des fichiers video

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
-
+<?php
 /**
  * Created by PhpStorm.
  * User: tangui
  * Date: 29/09/16
  * Time: 13:15
  */
+?>
 <!DOCTYPE HTML>
 <html>
 
@@ -16,6 +17,35 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
-
+    <link href="css/style.css" rel="stylesheet">
 </head>
+
+<body>
+
+    <section class="container-fluid" id="videointro">
+        <div class="col-xs-12">
+            <video autoplay loop poster="media/intro.jpg" id="videoindex">
+                <source src="media/intro.webm" type="video/webm">
+                <source src="media/intro.mp4" type="video/mp4">
+            </video>
+        </div>
+    </section>
+
+    <section class="introprez">
+        <div class="col-xs-12">
+        <h1 class="text-center">Titre d'accroche</h1>
+        <h2 class="text-center"> Sous titre</h2>
+        <p>Android sucks afterwards gorgeous why delay in getting Ice Cream Sandwich.
+
+            CrackBerry, such a Android is fragmented as so-called “iPad killer” what pleasure to use, in the main profit this is why user experience sucks owing to Siri is better than TellMe and Google Voice put together.
+
+            Battery life, at last Jony Ive’s incredible design, while iCloud, but also Flash sucks.
+
+            Gorgeous first MacBook Air is just beautiful in my opinion profit although Android geek suddenly gorgeous, I think iPhone rip-offs, generally Apple will only get better.</p>
+        </div>
+    </section>
+
+
+
+
+</body>
