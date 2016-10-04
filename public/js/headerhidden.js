a86df6313ac975ccec96876ff42d06976ce0dfca
tanguilb <tangui.lebourdonnec@gmail.com>
2016-10-04 08:37:48 +0200

    resolution du bug de transparence en haut de page du header

--- public/js/headerhidden.js
+++ public/js/headerhidden.js
@@ -9,28 +9,33 @@
         $navbar = $(".navbar-inverse"),
         $elements = $('body').children();
 
-
+        $navbar.css({                           // Met la bare de navigation en transparence
+        "background":"transparent",
+        "background-image":"none",
+        "border-color":"transparent",
+        "box-shadow" : "none"
+    })
 
     $window.scroll(function(){
         var scrollTop = $(this).scrollTop();
         $elements.each(function(){
             if (scrollTop < offset) {
-
-                $logo.show();
+                $logo.show();                   // Montre le logo et les réseaux sociaux lorsque l'on déscroll et la remet en transparence
                 social.show();
-
-
-
-
-
+                $navbar.css({
+                    "background":"transparent",
+                    "background-image":"none",
+                    "border-color":"transparent",
+                    "box-shadow" : "none"
+                })
             } else {
-                $logo.hide();
+                $logo.hide();                   // Cache le logo et les réseaux sociaux et remet la bar de navigation en noire
                 social.hide();
-                $navbar.css("background-color", "black");
-
+                $navbar.css({
+                    "background-color": "black",
+                    "height": "100px"
+                });
             }
         })
     });
-
-
 })(jQuery);
