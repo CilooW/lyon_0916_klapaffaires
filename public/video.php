a6510b44c2bc1985a04417ed6da6225ddb0b8207
Riad Hacini <riad.hacini.pro@gmail.com>
2016-10-05 15:52:38 +0200

    Sauvegarde avant merge

--- public/video.php
+++ public/video.php
@@ -21,7 +21,6 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="css/style.css" rel="stylesheet">
-
 </head>
 
 <body>
