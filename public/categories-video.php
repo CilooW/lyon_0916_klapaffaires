df0ff6ca0241a3705ab724e905fe9e34b6594ebe
Riad Hacini <riad.hacini.pro@gmail.com>
2016-10-12 15:58:13 +0200

    Commit avant merge Coherence CSS et category video

--- public/categories-video.php
+++ public/categories-video.php
@@ -14,6 +14,7 @@
     <meta charset="utf-8">
     <meta name="Klap Affaires Publireportage Vidéo à Lyon">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="icon" type="image/png" href="img/favico.png" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
