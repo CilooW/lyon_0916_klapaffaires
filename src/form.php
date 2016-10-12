d84206061fa15fc580c89cab4692c703adf97d3d
Nissa69 <yann.chanteloup.pro@gmail.com>
2016-10-12 11:34:08 +0200

    Formulaire et prestation ok

--- src/form.php
+++ src/form.php
@@ -8,9 +8,11 @@
 
 include ("bdd.php");
 
-$sql = "INSERT INTO formulaire(Nom, Prenom, Email, Societe, Message) VALUES ('$_POST[Nom]','$_POST[Prenom]','$_POST[Email]','$_POST[Societe]','$_POST[Message]')";
-$con = getConnexion();
-$result = execSql($con, $sql);
+if(!empty($_POST['Nom']) && !empty($_POST['Prenom']) && !empty($_POST['Email']) && !empty($_POST['Societe']) && !empty($_POST['Message'])) {
 
-header('location: ../public/formulaire.php');
+    $sql = "INSERT INTO formulaire(Nom, Prenom, Email, Societe, Message) VALUES ('$_POST[Nom]','$_POST[Prenom]','$_POST[Email]','$_POST[Societe]','$_POST[Message]')";
+    $con = getConnexion();
+    $result = execSql($con, $sql);
 
+    header('location: ../public/formulaire.php');
+}
