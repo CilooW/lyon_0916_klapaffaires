0c2d892b38c0ac64b87d24b5911aafdae681a52d
tanguilb <tangui.lebourdonnec@gmail.com>
2016-10-24 14:39:21 +0200

    add a login for admin dashboard and encrypting the password

--- src/KlapBundle/Controller/DefaultController.php
+++ src/KlapBundle/Controller/DefaultController.php
@@ -2,12 +2,22 @@
 
 namespace KlapBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('KlapBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @return Response
+     */
+    public function adminAction()
+    {
+        return new Response('<html><body>Admin page!</body></html>');
     }
 }
